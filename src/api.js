0d9c8b6c140c70de8c44e977c8e9754bbdb9b0b7
Udit Chaudhary <71772683+uditschoolboy@users.noreply.github.com>
2021-10-19 13:01:51 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -6,41 +6,6 @@ const getComments = async() => {
             userName: "Udit Chaudhary",
             text: "This is the first comment",
             parentId: null
-        },
-        {
-            id: "2",
-            userId: "2",
-            userName: "Shambhavi Shukla",
-            text: "This is the second Comment",
-            parentId: null
-        },
-        {
-            id: "3",
-            userId: "1",
-            userName: "Udit Chaudhary",
-            text: "Hi Dear!",
-            parentId: "2"
-        },
-        {
-            id: "4",
-            userId: "2",
-            userName: "Shambhavi Shukla",
-            text: "Hi my love",
-            parentId: "3"
-        },
-        {
-            id: "5",
-            userId: "2",
-            userName: "Shambhavi Shukla",
-            text: "Wow! Marry Me",
-            parentId: "1"
-        },
-        {
-            id: "6",
-            userId: "1",
-            userName: "Udit Chaudhary",
-            text: "Sure my baby",
-            parentId: "5"
         }
     ];
 }
